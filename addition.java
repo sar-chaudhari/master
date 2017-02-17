8f3d718028a88118c79d49dbfd659108cf8017a4
sar-chaudhari <sar.chaudhari@gmail.com>
2017-02-17 18:28:46 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,13 @@
+class addition
+{
+public static void main (String []args)
+{double a,b,c,d,e,f;
+a=10; b=20;
+c=a+b;
+d=b-a;
+e=a*b; 
+f=a/b;
+System.out.println("\n sum of two numbers = " + c + "\n" + "\n Difference numbers = " + d+"\n"+"\n mul'n of numbers = " + e+"\n"+"\n div's numbers = "+f);
+//System.out.println();
+}
+}
